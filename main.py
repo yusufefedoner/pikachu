01d6bbb994519a973cd97cb5a1a3634d570895ab
YUSUFÇUK <yusufefedoner@gmail.com>
2025-11-11 11:26:05 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+from config import token
+from logic import Pokemon
+
+# Bot için yetkileri/intents ayarlama
+intents = discord.Intents.default()  # Varsayılan ayarların alınması
+intents.messages = True              # Botun mesajları işlemesine izin verme
+intents.message_content = True       # Botun mesaj içeriğini okumasına izin verme
+intents.guilds = True                # Botun sunucularla çalışmasına izin verme
+
+# Tanımlanmış bir komut önekine ve etkinleştirilmiş amaçlara sahip bir bot oluşturma
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Bot çalışmaya hazır olduğunda tetiklenen bir olay
+@bot.event
+async def on_ready():
+    print(f'Giriş yapıldı:  {bot.user.name}')  # Botun adını konsola çıktı olarak verir
+
+# '!go' komutu
+@bot.command()
+async def go(ctx):
+    author = ctx.author.name  # Mesaj yazarının adını alma
+    # Kullanıcının zaten bir Pokémon'u olup olmadığını kontrol edin. Eğer yoksa, o zaman...
+    if author not in Pokemon.pokemons.keys():
+        pokemon = Pokemon(author)  # Yeni bir Pokémon oluşturma
+        await ctx.send(await pokemon.info())  # Pokémon hakkında bilgi gönderilmesi
+        image_url = await pokemon.show_img()  # Pokémon resminin URL'sini alma
+        if image_url:
+            embed = discord.Embed()  # Gömülü mesajı oluşturma
+            embed.set_image(url=image_url)  # Pokémon'un görüntüsünün ayarlanması
+            await ctx.send(embed=embed)  # Görüntü içeren gömülü bir mesaj gönderme
+        else:
+            await ctx.send("Pokémonun görüntüsü yüklenemedi!")
+    else:
+        await ctx.send("Zaten kendi Pokémonunuzu oluşturdunuz!")  # Bir Pokémon'un daha önce oluşturulup oluşturulmadığını gösteren bir mesaj
+# Botun çalıştırılması
+bot.run(token)
